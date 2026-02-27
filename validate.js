b22d3786dfd657b7ffdc572aaa5f75862cca23c6
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-02-27 12:05:18 +0530

    TE-T291 Now USer Can Add Special Characters in productName and productDescription

--- validate.js
+++ validate.js
@@ -4,8 +4,8 @@ export function validateProduct({ pId, pName, file, price, des }) {
     return false;
   }
 
-  if (!pName || !/^[a-zA-Z0-9\s]+$/.test(pName)) {
-    alert("Product Name must contain only letters and numbers");
+  if (!pName || !/^[a-zA-Z0-9\s@&._-]+$/.test(pName)) {
+    alert("Product Name Contains Invalid Characters");
     return false;
   }
 
@@ -20,7 +20,7 @@ export function validateProduct({ pId, pName, file, price, des }) {
     alert("Only JPG, PNG and WEBP image files are allowed");
     return false;
   }
-  
+
   if (file.size > 2 * 1024 * 1024) {
     alert("Image size must be less than 2MB");
     return false;
@@ -31,7 +31,7 @@ export function validateProduct({ pId, pName, file, price, des }) {
     return false;
   }
 
-  if (!des || !/^[a-zA-Z0-9\s.,!]+$/.test(des)) {
+  if (!des || !/^[a-zA-Z0-9\s@&.,!#%()/_-]+$/.test(des)) {
     alert("Description contains invalid characters");
     return false;
   }
