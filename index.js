d9333fb347e9e500ae82ac6d459d4feef0be1bfc
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-03-11 12:48:48 +0530

    TE-T291 converted js into jquery

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
 import { validateProduct } from "./validate.js";
-let saveBtn = document.getElementById("save-btn");
-import { getProducts,setProducts } from "./helper.js";
+import { getProducts, setProducts } from "./helper.js";
 
-//Clears the input field when form is submitted
-let clearInputFiels = () => {
-  document.getElementById("productId").value = "";
-  document.getElementById("productName").value = "";
-  document.getElementById("imageUrl").value = "";
-  document.getElementById("price").value = "";
-  document.getElementById("description").value = "";
-};
+$(function () {
+  //Clears the input field when form is submitted
+  const clearInputFiels = function () {
+    $("#productId").val("");
+    $("#productName").val("");
+    $("#imageUrl").val("");
+    $("#price").val("");
+    $("#description").val("");
+  };
 
-if (saveBtn) {
-  saveBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+  if ($("#save-btn")) {
+    $("#save-btn").click(function (e) {
+      e.preventDefault();
+      let pId = $("#productId").val().trim();
+      let pName = $("#productName").val().trim();
+      let imageInput = $("#imageUrl");
+      let price = $("#price").val().trim();
+      let des = $("#description").val().trim();
+      let file = imageInput[0].files[0];
 
-    let pId = document.getElementById("productId").value.trim();
-    let pName = document.getElementById("productName").value.trim();
-    let imageInput = document.getElementById("imageUrl");
-    let price = document.getElementById("price").value.trim();
-    let des = document.getElementById("description").value.trim();
-    let file = imageInput.files[0];
-
-    let isValid = validateProduct({ pId, pName, file, price, des });
-    if (!isValid){
-      clearInputFiels();
-      return;
-    }
-
-    let reader = new FileReader();
-
-    reader.onload = function () {
-      let base64Image = reader.result;
-
-      let newProduct = {
-        productId: pId,
-        productName: pName,
-        productImage: base64Image,
-        productPrice: price,
-        productDescription: des,
-      };
-
-      let products = getProducts();
-
-      // Check if ID already exists
-      let idExist = products.reduce((acc, crr) => {
-        if (crr.productId == newProduct.productId) {
-          acc = true;
-        }
-        return acc;
-      }, false);
-
-      if (idExist) {
-        alert("Product With this Id Already Exist!!");
-      } else {
-        products.push(newProduct);
-        setProducts(products);
-        alert("Product Successfully Added!!");
+      let isValid = validateProduct({ pId, pName, file, price, des });
+      if (!isValid) {
         clearInputFiels();
-        window.location.href = "products.html";
+        return;
       }
-    };
 
-    reader.readAsDataURL(file);
-  });
-}
+      let reader = new FileReader();
+
+      reader.onload = function () {
+        let base64Image = reader.result;
+
+        let newProduct = {
+          productId: pId,
+          productName: pName,
+          productImage: base64Image,
+          productPrice: price,
+          productDescription: des,
+        };
+
+        let products = getProducts();
+
+        // Check if ID already exists
+        let idExist = products.reduce((acc, crr) => {
+          if (crr.productId == newProduct.productId) {
+            acc = true;
+          }
+          return acc;
+        }, false);
+
+        if (idExist) {
+          alert("Product With this Id Already Exist!!");
+        } else {
+          products.push(newProduct);
+          setProducts(products);
+          alert("Product Successfully Added!!");
+          clearInputFiels();
+          window.location.href = "products.html";
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+});
--- script.js
+++ script.js
@@ -2,165 +2,173 @@ import { getProducts, setProducts } from "./helper.js";
 import { validateProduct } from "./validate.js";
 import { sortProducts } from "./helper.js";
 
-function displayProducts( products = getProducts()) {
-  let productList = document.getElementById("productList");
-  if (products.length === 0 || null) {
-    productList.innerHTML = `
-    <center>
-    <h1 class="text-dark mt-5">No Products Added Please Add Products!!</h1>
-    </center>
-    `;
+function displayProducts(products = getProducts()) {
+  let productList = $("#productList");
+
+  if (!products || products.length === 0) {
+    productList.html(`
+      <center>
+        <h1 class="text-dark mt-5">No Products Added Please Add Products!!</h1>
+      </center>
+    `);
     return;
   }
 
-  productList.innerHTML = "";
+  productList.html("");
 
   products.forEach((product) => {
-    productList.innerHTML += `
-        <div class="card m-3" style="width:250px;">
-            <div class="card-body d-flex flex-column text-center">
-                <h5 class="card-title">${product.productName}</h5>
-                <img src="${product.productImage}" class="img-fluid mb-2" height="100">
-                <p><strong>Price:</strong> ${product.productPrice}</p>
-                <p class="flex-grow-1 fw-bold">Product Id: ${product.productId}</p>
-                <p class="flex-grow-1">${product.productDescription}</p>
+    productList.append(`
+      <div class="card m-3" style="width:250px;">
+        <div class="card-body d-flex flex-column text-center">
+          <h5 class="card-title">${product.productName}</h5>
+          <img src="${product.productImage}" class="img-fluid mb-2" height="100">
+          <p><strong>Price:</strong> ${product.productPrice}</p>
+          <p class="flex-grow-1 fw-bold">Product Id: ${product.productId}</p>
+          <p class="flex-grow-1">${product.productDescription}</p>
 
-                <div class="d-flex justify-content-between mt-auto">
-                    <button class="btn btn-danger btn-sm" onclick="deleteProduct(${product.productId})">Delete</button>
-                    <button class="btn btn-warning btn-sm" onClick="editProduct(${product.productId})">Edit</button>
-                </div>
-            </div>
+          <div class="d-flex justify-content-between mt-auto">
+            <button class="btn btn-danger btn-sm" onclick="deleteProduct(${product.productId})">Delete</button>
+            <button class="btn btn-warning btn-sm" onclick="editProduct(${product.productId})">Edit</button>
+          </div>
         </div>
-    `;
+      </div>
+    `);
   });
 }
 
-displayProducts();
-
-//We Can' Acces Globally Created onclick listeners So we have to add winodw to use it
-window.deleteProduct = function (id) {
-  if (!confirm("Are you sure you want to delete this product?")) {
-    return;
-  }
-  let products = getProducts();
-  const updatedProducts = products.filter((p) => id != p.productId); //Keep The element Which Return True Removes Element Which Returns False That Got Removed From Object Array
-
-  setProducts(updatedProducts);
+$(function () {
   displayProducts();
-  alert("Product Deleted!!");
-};
 
+  //We Can' Acces Globally Created onclick listeners So we have to add winodw to use it
+  window.deleteProduct = function (id) {
+    if (!confirm("Are you sure you want to delete this product?")) {
+      return;
+    }
 
+    let products = getProducts();
 
-//We Can' Acces Globally Created onclick listeners So we have to add winodw to use it
-window.editProduct = function (id) {
-  let products = getProducts();
-  let product = products.filter((p) => p.productId == id)[0]; //Returns Us An Object So We Take Only The first Array element From it
-  if (!product) return;
-
-  document.getElementById("editProductId").value = product.productId;
-  document.getElementById("editProductName").value = product.productName;
-  document.getElementById("editPrice").value = product.productPrice;
-  document.getElementById("editDescription").value = product.productDescription;
-
-  // Clear file input every time modal opens
-  document.getElementById("editFile").value = "";
-
-  //Model shows after The Value is Set
-  let modal = new bootstrap.Modal(document.getElementById("editModal"));
-  modal.show();
-};
-
-let form = document.getElementById("editForm");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  let id = document.getElementById("editProductId").value; // ID is fixed
-  let name = document.getElementById("editProductName").value.trim();
-  let price = document.getElementById("editPrice").value.trim();
-  let description = document.getElementById("editDescription").value.trim();
-  let fileInput = document.getElementById("editFile");
-  let file = fileInput.files[0]; // Gets The Fisrt File Selected by User If User Selects Multiple Files
-
-  let isValid = validateProduct({
-    pId: id,
-    pName: name,
-    file: file,
-    price: price,
-    des: description,
-    isEdit: true,
-
-  }); // {size: 0 handle Optional Image}
-  if (!isValid) return;
-
-  let products = getProducts();
-
-  let currentProduct = products.filter((p) => p.productId == id)[0]; //Returns Us An Object So We Take Only The first Array element From it
-  if (!currentProduct) return;
-
-  function saveProduct(finalImage) {
-    // Remove old product
-    let updatedProducts = products.filter((p) => p.productId != id);
-
-    // Push updated product
-    updatedProducts.push({
-      productId: id,
-      productName: name,
-      productPrice: price,
-      productDescription: description,
-      productImage: finalImage,
-    });
+    const updatedProducts = products.filter((p) => id != p.productId);
 
     setProducts(updatedProducts);
-
-    alert("Product Updated Successfully!");
     displayProducts();
 
-    // Get the existing Bootstrap modal instance of #editModal
-    // (used to control the modal like hide(), show(), etc.)
-    let modal = bootstrap.Modal.getInstance(
-      document.getElementById("editModal"),
-    );
-    modal.hide();
+    alert("Product Deleted!!");
+  };
+
+  //We Can' Acces Globally Created onclick listeners So we have to add winodw to use it
+  window.editProduct = function (id) {
+    let products = getProducts();
+
+    let product = products.find((p) => p.productId == id);
+
+    if (!product) return;
+
+    $("#editProductId").val(product.productId);
+    $("#editProductName").val(product.productName);
+    $("#editPrice").val(product.productPrice);
+    $("#editDescription").val(product.productDescription);
+
+    $("#editFile").val("");
+
+    let modal = new bootstrap.Modal($("#editModal")[0]);
+    modal.show();
+  };
+
+  // Edit Form Submit
+  $("#editForm").on("submit", function (e) {
+    e.preventDefault();
+
+    let id = $("#editProductId").val();
+    let name = $("#editProductName").val().trim();
+    let price = $("#editPrice").val().trim();
+    let description = $("#editDescription").val().trim();
+
+    let fileInput = $("#editFile")[0];
+    let file = fileInput.files[0];
+
+    let isValid = validateProduct({
+      pId: id,
+      pName: name,
+      file: file,
+      price: price,
+      des: description,
+      isEdit: true,
+    });
+
+    if (!isValid) return;
+
+    let products = getProducts();
+
+    let currentProduct = products.find((p) => p.productId == id);
+    if (!currentProduct) return;
+
+    function saveProduct(finalImage) {
+      let updatedProducts = products.filter((p) => p.productId != id);
+
+      updatedProducts.push({
+        productId: id,
+        productName: name,
+        productPrice: price,
+        productDescription: description,
+        productImage: finalImage,
+      });
+
+      setProducts(updatedProducts);
+
+      alert("Product Updated Successfully!");
+
+      displayProducts();
+
+      let modal = bootstrap.Modal.getInstance($("#editModal")[0]);
+      modal.hide();
+    }
+
+    if (!file) {
+      saveProduct(currentProduct.productImage);
+    } else {
+      let reader = new FileReader();
+
+      reader.onload = function () {
+        saveProduct(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  // Sort Filter
+  let sortFilter = $("#sortFilter");
+
+  if (sortFilter.length) {
+    sortFilter.on("change", function () {
+      let products = getProducts();
+      let value = $(this).val();
+
+      const sortedProducts = sortProducts(products, value);
+
+      displayProducts(sortedProducts);
+    });
   }
 
-  // If new image selected
-  if (!file) {
-    saveProduct(currentProduct.productImage);
-  } else {
-    let reader = new FileReader();
-    reader.onload = function () {
-      saveProduct(reader.result);
-    };
-    reader.readAsDataURL(file);
+  // Search Filter
+  let searchInput = $("#searchInput");
+
+  if (searchInput.length) {
+    searchInput.on("input", function () {
+      let value = $(this).val().trim();
+
+      let products = getProducts();
+
+      if (!value) {
+        displayProducts(products);
+        return;
+      }
+
+      let filteredProducts = products.filter((p) =>
+        p.productId.includes(value),
+      );
+
+      displayProducts(filteredProducts);
+    });
   }
 });
-
-//Event Deligation Put Event Listner On Parent (Bubbling)
-
-let sortFilter = document.getElementById("sortFilter");
-if (sortFilter) {
-  sortFilter.addEventListener("change", function () {
-    let products = getProducts();
-    let value = this.value;
-    const sortedProducts = sortProducts(products, value);
-    displayProducts(sortedProducts);
-  });
-}
-
-//Add Search Filter Logic
-
-let searchInput = document.getElementById('searchInput')
-
-if(searchInput){
-searchInput.addEventListener('input',function(e){
-  let value = e.target.value.trim()
-  let products = getProducts()
-  if(!value){
-    displayProducts(products);
-    return;
-  }
-  let filteredProducts = products.filter((p)=>p.productId.includes(value));
-  displayProducts(filteredProducts)
-})
-}
